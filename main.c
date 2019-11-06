8a52c9121cd32e3bab329ef9d2b2dcb83997a087
Efe Can Cevher <37309169+efe-cevher@users.noreply.github.com>
2019-11-06 22:36:12 +0300

    structure, bugfixes

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#include"transaction.h"
 
 
 void readFile(const char* fileName){
@@ -7,27 +9,58 @@ void readFile(const char* fileName){
     FILE *file;
     file = fopen(fileName, "read");
 
+    
     if(file == NULL){
         printf("Error! File not found.");   
         exit(1);
     }
     char c;
     int wordLen = 0;
+    int wordNo = 0;
+    int lineNo = 0;
     char tmp[63];
+    Transaction* transactions[100];
 
-    
 
     while((c = fgetc(file)) != EOF){
-        
-        if(c == ','){
-            tmp[wordLen] = '\0';
-            printf("%s, ", tmp);
-            wordLen = 0;
+
+        if(c == ' '){
             continue;
         }
-        if(c == '\n'){
-            printf("YAY");
+
+        if(c == ',' || c == '\n'){
+
+            tmp[wordLen] = '\0';
+
+            
+
+            if(wordNo == 0){
+                transactions[lineNo] = (Transaction*)malloc(sizeof(Transaction*));
+                char subStr[wordLen - 4];
+                memcpy(subStr, &tmp[4], wordLen - 4);
+                transactions[lineNo]->day = atoi(subStr);
+                printf("%d", atoi(subStr));
+            }else if (wordNo == 1){
+                char subStr[wordLen - 9];
+                memcpy(subStr, &tmp[9], wordLen - 9);
+                transactions[lineNo]->customerId = atoi(subStr);
+            }else{
+                transactions[lineNo]->items = (char**) malloc(sizeof(char*) * 64);
+                transactions[lineNo]->items[wordNo - 2] = tmp; 
+            }
+
+            if(c == '\n'){
+                wordNo = 0;
+                wordLen = 0;
+                lineNo++;
+                continue;
+            }
+            
+            wordLen = 0;
+            wordNo++;
+            continue;
         }
+        
         tmp[wordLen] = c;
         wordLen++;
         
---
+++ transaction.h
@@ -0,0 +1,7 @@
+struct transaction{
+    int day;
+    int customerId;
+    char** items;
+};
+
+typedef struct transaction Transaction;
